ef95775ed8e4f770e2225180df0d9397db85c4cf
Lenix <lenix01x@gmail.com>
2025-10-25 13:12:50 +0100

    update: support the client exports

--- modules/pedestrians/client.js
+++ modules/pedestrians/client.js
@@ -1,4 +1,8 @@
-exports.tr_lib.callback().register('getPedScenario', (ped, scenario) => {
+function callback() {
+    return exports.tr_lib.callback()
+}
+
+callback().register('getPedScenario', (ped, scenario) => {
     try {
         TaskStartScenarioInPlace(ped, scenario.name, scenario.timeToLeave, scenario.playIntroClip);
         return [true, 'success'];
@@ -6,3 +10,24 @@ exports.tr_lib.callback().register('getPedScenario', (ped, scenario) => {
         return [false, error.message];
     }
 });
+
+function createSinglePed(model, coords, scenario, isAccessPublic, isControlPublic) {
+    return callback().awaitServer('createSinglePed', null, model, coords, scenario, isAccessPublic, isControlPublic)
+}
+
+function createMultiplePeds(peds, defaultSettings) {
+    return callback().awaitServer('createMultiplePeds', null, peds, defaultSettings)
+}
+
+function clearCreatedPed(entity) {
+    return callback().awaitServer('clearCreatedPed', null, entity)
+}
+
+function clearCreatedPeds(entities) {
+    return callback().awaitServer('clearCreatedPeds', null, entities)
+}
+
+exports('createSinglePed', createSinglePed)
+exports('createMultiplePeds', createMultiplePeds)
+exports('clearCreatedPed', clearCreatedPed)
+exports('clearCreatedPeds', clearCreatedPeds)
